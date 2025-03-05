0e0fadcb4f4a699ebea0a220011d3a3d2c49ba95
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-03-05 09:29:28 -0600

    clean up code

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,19 +1,23 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
 
+function frontPad(input, char = "0", len = 2) {
+  let str = String(input);
+  while (str.length < len) {
+    str = char + str;
+  }
+  return str;
+}
+
 export default function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
   eleventyConfig.addPassthroughCopy("*.css");
   eleventyConfig.addPassthroughCopy("posts/**/img/*");
 
   eleventyConfig.addFilter("dateString", function (date) {
-    function frontPad(input, char = "0", len = 2) {
-      let str = String(input);
-      while (str.length < len) {
-        str = char + str;
-      }
-      return str;
-    }
+    const year = date.getUTCFullYear();
+    const month = frontPad(date.getUTCMonth() + 1);
+    const day = frontPad(date.getUTCDate());
 
-    return `${date.getUTCFullYear()}-${frontPad(date.getUTCMonth() + 1)}-${frontPad(date.getUTCDate())}`;
+    return `${year}-${month}-${day}`;
   });
 }
