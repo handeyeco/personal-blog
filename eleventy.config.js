4777e089e12b96f8530c46aaf7231ca309379854
Matthew Curtis <matthewcurtis@khanacademy.org>
2025-10-01 10:08:33 -0500

    add RSS/Atom feed

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,5 @@
 import { EleventyHtmlBasePlugin } from "@11ty/eleventy";
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 function frontPad(input, char = "0", len = 2) {
   let str = String(input);
@@ -13,11 +14,32 @@ export default function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("*.css");
   eleventyConfig.addPassthroughCopy("posts/**/img/*");
 
+  // date transform
   eleventyConfig.addFilter("dateString", function (date) {
     const year = date.getUTCFullYear();
     const month = frontPad(date.getUTCMonth() + 1);
     const day = frontPad(date.getUTCDate());
 
     return `${year}-${month}-${day}`;
+  });
+
+  // RSS feed
+  eleventyConfig.addPlugin(feedPlugin, {
+    type: "atom", // or "rss", "json"
+    outputPath: "/feed.xml",
+    collection: {
+      name: "posts", // iterate over `collections.posts`
+      limit: 10, // 0 means no limit
+    },
+    metadata: {
+      language: "en",
+      title: "Aimless Blog",
+      subtitle: "Miscellaneous thoughts, news, and music picks.",
+      base: "https://handeyeco.github.io/personal-blog/",
+      author: {
+        name: "Aimless Blog",
+        email: "", // Optional
+      },
+    },
   });
 }
